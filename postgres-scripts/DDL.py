096b8e7fe590eb2c29bc0637d9b4bd312d0b1124
unknown <dhoifullah.luthmajied05@gmail.com>
2025-12-20 13:14:16 +0700

    firsts commit

--- postgres-scripts/DDL.py
+++ postgres-scripts/DDL.py
@@ -12,94 +12,125 @@ conn = psycopg2.connect(**DB_CONFIG)
 conn.autocommit = True
 cur = conn.cursor()
 
-def create_tables():
+
+def create_bronze_tables():
     queries = [
 
         # =====================
-        # CUSTOMER
+        # BRONZE CUSTOMER RAW
         # =====================
         """
-        CREATE TABLE IF NOT EXISTS customer (
-            customer_id SERIAL PRIMARY KEY,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            full_name VARCHAR(100),
-            name_id VARCHAR(50),
-            place_of_birth VARCHAR(50),
-            date_of_birth DATE,
-            gender CHAR(1),
-            email VARCHAR(100),
-            phone VARCHAR(30),
-            address TEXT,
-            city VARCHAR(50),
-            postal_code VARCHAR(10),
-            registration TIMESTAMP
+        CREATE TABLE IF NOT EXISTS bronze_customer_raw (
+            _op_type            VARCHAR(5),
+            _is_deleted         BOOLEAN DEFAULT FALSE,
+            _source_system      VARCHAR(50),
+            _topic_name         VARCHAR(100),
+            _ingestion_time     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            _load_date          DATE DEFAULT CURRENT_DATE,
+            _row_hash           VARCHAR(64),
+            customer_id         VARCHAR(50),
+            date                VARCHAR(50),
+            full_name           VARCHAR(100),
+            name_id             VARCHAR(50),
+            place_of_birth      VARCHAR(50),
+            date_of_birth       VARCHAR(50),
+            gender              VARCHAR(10),
+            email               VARCHAR(100),
+            phone               VARCHAR(30),
+            address             TEXT,
+            city                VARCHAR(50),
+            postal_code         VARCHAR(10),
+            registration        VARCHAR(50)
         );
         """,
 
         # =====================
-        # PRODUCT
+        # BRONZE PRODUCT RAW
         # =====================
         """
-        CREATE TABLE IF NOT EXISTS product (
-            product_id SERIAL PRIMARY KEY,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            product_name VARCHAR(100),
-            category VARCHAR(50),
-            brand VARCHAR(50),
-            base_price NUMERIC(12,2)
+        CREATE TABLE IF NOT EXISTS bronze_product_raw (
+            _op_type            VARCHAR(5),
+            _is_deleted         BOOLEAN DEFAULT FALSE,
+            _source_system      VARCHAR(50),
+            _topic_name         VARCHAR(100),
+            _ingestion_time     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            _load_date          DATE DEFAULT CURRENT_DATE,
+            _row_hash           VARCHAR(64),
+            product_id          VARCHAR(50),
+            date                VARCHAR(50),
+            product_name        VARCHAR(100),
+            category            VARCHAR(50),
+            brand               VARCHAR(50),
+            base_price          VARCHAR(50)
         );
         """,
 
         # =====================
-        # SHIPPING
+        # BRONZE SHIPPING RAW
         # =====================
         """
-        CREATE TABLE IF NOT EXISTS shipping (
-            shipping_id SERIAL PRIMARY KEY,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            courier VARCHAR(30),
-            pick_up VARCHAR(50),
-            delivery VARCHAR(50),
-            shipping_status VARCHAR(20),
-            shipping_cost NUMERIC(12,2)
+        CREATE TABLE IF NOT EXISTS bronze_shipping_raw (
+            _op_type            VARCHAR(5),
+            _is_deleted         BOOLEAN DEFAULT FALSE,
+            _source_system      VARCHAR(50),
+            _topic_name         VARCHAR(100),
+            _ingestion_time     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            _load_date          DATE DEFAULT CURRENT_DATE,
+            _row_hash           VARCHAR(64),
+            shipping_id         VARCHAR(50),
+            date                VARCHAR(50),
+            courier             VARCHAR(30),
+            pick_up             VARCHAR(50),
+            delivery            VARCHAR(50),
+            shipping_status     VARCHAR(30),
+            shipping_cost       VARCHAR(50)
         );
         """,
 
         # =====================
-        # ORDERS (FACT)
+        # BRONZE PAYMENTS RAW
         # =====================
         """
-        CREATE TABLE IF NOT EXISTS orders (
-            order_id SERIAL PRIMARY KEY,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            customer_id INT,
-            shipping_id INT,
-            product_id INT,
-            quantity INT,
-            item_price NUMERIC(12,2),
-            subtotal NUMERIC(12,2),
-            discount NUMERIC(12,2),
-            total_amount NUMERIC(12,2),
-
-            CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-            CONSTRAINT fk_shipping FOREIGN KEY (shipping_id) REFERENCES shipping(shipping_id),
-            CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product(product_id)
+        CREATE TABLE IF NOT EXISTS bronze_payments_raw (
+            _op_type            VARCHAR(5),
+            _is_deleted         BOOLEAN DEFAULT FALSE,
+            _source_system      VARCHAR(50),
+            _topic_name         VARCHAR(100),
+            _ingestion_time     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            _load_date          DATE DEFAULT CURRENT_DATE,
+            _row_hash           VARCHAR(64),
+            payment_id          VARCHAR(50),
+            date                VARCHAR(50),
+            order_id            VARCHAR(50),
+            payment_method      VARCHAR(30),
+            payment_status      VARCHAR(30),
+            amount              VARCHAR(50)
         );
         """,
 
         # =====================
-        # PAYMENTS
+        # BRONZE ORDERS RAW
         # =====================
         """
-        CREATE TABLE IF NOT EXISTS payments (
-            payment_id SERIAL PRIMARY KEY,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            order_id INT,
-            payment_method VARCHAR(30),
-            payment_status VARCHAR(20),
-            amount NUMERIC(12,2),
-
-            CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES orders(order_id)
+        CREATE TABLE IF NOT EXISTS bronze_orders_raw (
+            _op_type            VARCHAR(5),
+            _is_deleted         BOOLEAN DEFAULT FALSE,
+            _source_system      VARCHAR(50),
+            _topic_name         VARCHAR(100),
+            _ingestion_time     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            _load_date          DATE DEFAULT CURRENT_DATE,
+            _row_hash           VARCHAR(64),
+            order_id            VARCHAR(50),
+            date                VARCHAR(50),
+            customer_id         VARCHAR(50),
+            shipping_id         VARCHAR(50),
+            payment_id          VARCHAR(50),
+            product_id          VARCHAR(50),
+            quantity            VARCHAR(50),
+            item_price          VARCHAR(50),
+            subtotal            VARCHAR(50),
+            discount            VARCHAR(50),
+            total_amount        VARCHAR(50)
         );
         """
     ]
@@ -107,9 +138,10 @@ def create_tables():
     for q in queries:
         cur.execute(q)
 
-    print("✅ All tables created successfully")
+    print("✅ Bronze RAW tables created successfully")
+
 
 if __name__ == "__main__":
-    create_tables()
+    create_bronze_tables()
     cur.close()
     conn.close()
--- postgres-scripts/DML.py
+++ postgres-scripts/DML.py
@@ -2,7 +2,9 @@ import psycopg2
 from faker import Faker
 import random
 import time
+import hashlib
 from datetime import datetime
+import uuid
 
 # =====================
 # CONFIG
@@ -16,6 +18,8 @@ DB_CONFIG = {
 }
 
 fake = Faker("id_ID")
+SOURCE_SYSTEM = "faker_app"
+TOPIC_PREFIX = "bronze_topic"
 
 # =====================
 # CONNECT DB
@@ -24,132 +28,199 @@ conn = psycopg2.connect(**DB_CONFIG)
 conn.autocommit = True
 cur = conn.cursor()
 
-# =====================
-# INSERT FUNCTIONS
-# =====================
 
-def insert_customer():
-    query = """
-    INSERT INTO customer (
-        full_name, name_id, place_of_birth, date_of_birth,
-        gender, email, phone, address, city, postal_code, registration
-    ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-    RETURNING customer_id
-    """
-    cur.execute(query, (
+# =====================
+# HELPER
+# =====================
+def generate_hash(*values):
+    raw = "|".join([str(v) for v in values])
+    return hashlib.sha256(raw.encode()).hexdigest()
+
+
+# =====================
+# INSERT BRONZE CUSTOMER
+# =====================
+def insert_bronze_customer():
+    customer_id = str(uuid.uuid4())
+
+    values = (
+        customer_id,
+        datetime.now().strftime("%Y-%m-%d"),
         fake.name(),
         fake.uuid4(),
         fake.city(),
-        fake.date_of_birth(minimum_age=18, maximum_age=60),
+        fake.date_of_birth(minimum_age=18, maximum_age=60).strftime("%Y-%m-%d"),
         random.choice(["M", "F"]),
         fake.email(),
         fake.phone_number(),
         fake.address(),
         fake.city(),
         fake.postcode(),
-        datetime.now()
-    ))
-    return cur.fetchone()[0]
+        datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    )
 
+    row_hash = generate_hash(*values)
 
-def insert_product():
     query = """
-    INSERT INTO product (
-        product_name, category, brand, base_price
-    ) VALUES (%s,%s,%s,%s)
-    RETURNING product_id
+    INSERT INTO bronze_customer_raw (
+        customer_id, date, full_name, name_id, place_of_birth,
+        date_of_birth, gender, email, phone, address, city,
+        postal_code, registration,
+        _op_type, _source_system, _topic_name, _row_hash
+    ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
     """
-    cur.execute(query, (
+
+    cur.execute(query, (*values, "I", SOURCE_SYSTEM, f"{TOPIC_PREFIX}.customer", row_hash))
+
+    return customer_id
+
+
+# =====================
+# INSERT BRONZE PRODUCT
+# =====================
+def insert_bronze_product():
+    product_id = str(uuid.uuid4())
+
+    values = (
+        product_id,
+        datetime.now().strftime("%Y-%m-%d"),
         fake.word().title(),
         random.choice(["Electronics", "Fashion", "Food", "Books"]),
         random.choice(["Samsung", "Apple", "Xiaomi", "Nike", "Adidas"]),
-        random.randint(50_000, 5_000_000)
-    ))
-    return cur.fetchone()[0]
+        str(random.randint(50_000, 5_000_000))
+    )
 
+    row_hash = generate_hash(*values)
 
-def insert_shipping():
     query = """
-    INSERT INTO shipping (
-        courier, pick_up, delivery, shipping_status, shipping_cost
-    ) VALUES (%s,%s,%s,%s,%s)
-    RETURNING shipping_id
+    INSERT INTO bronze_product_raw (
+        product_id, date, product_name, category, brand, base_price,
+        _op_type, _source_system, _topic_name, _row_hash
+    ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
     """
-    cur.execute(query, (
+
+    cur.execute(query, (*values, "I", SOURCE_SYSTEM, f"{TOPIC_PREFIX}.product", row_hash))
+
+    return product_id
+
+
+# =====================
+# INSERT BRONZE SHIPPING
+# =====================
+def insert_bronze_shipping():
+    shipping_id = str(uuid.uuid4())
+
+    values = (
+        shipping_id,
+        datetime.now().strftime("%Y-%m-%d"),
         random.choice(["JNE", "J&T", "SiCepat", "AnterAja"]),
         fake.city(),
         fake.city(),
         random.choice(["CREATED", "IN_TRANSIT", "DELIVERED"]),
-        random.randint(10_000, 50_000)
-    ))
-    return cur.fetchone()[0]
+        str(random.randint(10_000, 50_000))
+    )
+
+    row_hash = generate_hash(*values)
+
+    query = """
+    INSERT INTO bronze_shipping_raw (
+        shipping_id, date, courier, pick_up, delivery,
+        shipping_status, shipping_cost,
+        _op_type, _source_system, _topic_name, _row_hash
+    ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+    """
+
+    cur.execute(query, (*values, "I", SOURCE_SYSTEM, f"{TOPIC_PREFIX}.shipping", row_hash))
+
+    return shipping_id
 
 
-def insert_order(customer_id, product_id, shipping_id):
+# =====================
+# INSERT BRONZE ORDER
+# =====================
+def insert_bronze_order(customer_id, product_id, shipping_id):
+    order_id = str(uuid.uuid4())
+
     qty = random.randint(1, 5)
     price = random.randint(50_000, 5_000_000)
     subtotal = qty * price
     discount = random.randint(0, int(subtotal * 0.2))
     total = subtotal - discount
 
-    query = """
-    INSERT INTO orders (
-        customer_id, shipping_id, product_id,
-        quantity, item_price, subtotal, discount, total_amount
-    ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
-    RETURNING order_id
-    """
-    cur.execute(query, (
+    values = (
+        order_id,
+        datetime.now().strftime("%Y-%m-%d"),
         customer_id,
         shipping_id,
+        str(uuid.uuid4()),  # payment_id (sementara raw)
         product_id,
-        qty,
-        price,
-        subtotal,
-        discount,
-        total
-    ))
-    return cur.fetchone()[0]
+        str(qty),
+        str(price),
+        str(subtotal),
+        str(discount),
+        str(total)
+    )
 
+    row_hash = generate_hash(*values)
 
-def insert_payment(order_id):
     query = """
-    INSERT INTO payments (
-        order_id, payment_method, payment_status, amount
-    ) VALUES (%s,%s,%s,%s)
-    RETURNING payment_id
+    INSERT INTO bronze_orders_raw (
+        order_id, date, customer_id, shipping_id, payment_id,
+        product_id, quantity, item_price, subtotal,
+        discount, total_amount,
+        _op_type, _source_system, _topic_name, _row_hash
+    ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
     """
-    cur.execute(query, (
+
+    cur.execute(query, (*values, "I", SOURCE_SYSTEM, f"{TOPIC_PREFIX}.orders", row_hash))
+
+    return order_id
+
+
+# =====================
+# INSERT BRONZE PAYMENT
+# =====================
+def insert_bronze_payment(order_id):
+    payment_id = str(uuid.uuid4())
+
+    values = (
+        payment_id,
+        datetime.now().strftime("%Y-%m-%d"),
         order_id,
         random.choice(["TRANSFER", "QRIS", "COD", "EWALLET"]),
         random.choice(["PAID", "PENDING"]),
-        random.randint(50_000, 5_000_000)
-    ))
-    return cur.fetchone()[0]
+        str(random.randint(50_000, 5_000_000))
+    )
+
+    row_hash = generate_hash(*values)
+
+    query = """
+    INSERT INTO bronze_payments_raw (
+        payment_id, date, order_id,
+        payment_method, payment_status, amount,
+        _op_type, _source_system, _topic_name, _row_hash
+    ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+    """
+
+    cur.execute(query, (*values, "I", SOURCE_SYSTEM, f"{TOPIC_PREFIX}.payments", row_hash))
 
 
 # =====================
 # MAIN LOOP
 # =====================
 if __name__ == "__main__":
-    print("🚀 Starting fake data generator (CDC-ready)...")
+    print("🚀 Inserting data into BRONZE RAW tables...")
 
     try:
         while True:
-            customer_id = insert_customer()
-            product_id = insert_product()
-            shipping_id = insert_shipping()
-            order_id = insert_order(customer_id, product_id, shipping_id)
-            payment_id = insert_payment(order_id)
+            customer_id = insert_bronze_customer()
+            product_id = insert_bronze_product()
+            shipping_id = insert_bronze_shipping()
+            order_id = insert_bronze_order(customer_id, product_id, shipping_id)
+            insert_bronze_payment(order_id)
 
-            print(
-                f"✅ Order {order_id} | "
-                f"Customer {customer_id} | "
-                f"Product {product_id} | "
-                f"Payment {payment_id}"
-            )
+            print(f"✅ Bronze data inserted | order_id={order_id}")
 
-            # delay supaya CDC kelihatan real-time
             time.sleep(random.randint(2, 5))
 
     except KeyboardInterrupt:
